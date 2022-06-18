cb4c608017e5cfaaa77e4cccb091562c9169d52a
Oleh Talaver <o.talaver@easygenerator.com>
2022-06-18 13:04:08 +0300

    feat: profile file

---
+++ profile.main.sh
@@ -0,0 +1,7 @@
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export GOPRIVATE=githb.com/Vidzhel,github.com/LTD-VADYUS
+
+export PATH=$PATH:/usr/local/nvim/bin:/home/user/.local/bin
+
