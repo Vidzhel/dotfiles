03165713ece1fd7228c6bfa7b741c6f1243e3bf8
Oleh Talaver <olegtalaver@gmail.com>
2023-02-10 20:50:44 +0200

    chore: JAVA, GO sdks setup path

--- profile.main.sh
+++ profile.main.sh
@@ -1,4 +1,4 @@
-export GOROOT=/usr/local/go
+export GOROOT=$HOME/sdk/go
 export GOPATH=$HOME/go
 export GOBIN=$GOPATH/bin/
 export PATH=$PATH:$GOROOT/bin:$GOBIN
@@ -7,3 +7,7 @@ export GOPRIVATE=githb.com/Vidzhel,github.com/LTD-VADYUS
 export PATH=$PATH:/usr/local/nvim/bin:/home/user/.local/bin
 
 export EDITOR=lvim
+
+export JAVA_HOME=$HOME/.jdks/openjdk-19.0.2
+export M2_HOME=/opt/apache-maven-3.8.7
+export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin
